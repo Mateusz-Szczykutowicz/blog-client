bc08f2a365e1049d557a53dc41b2c0395eed237a
Mateusz <mateuszszczykutowicz@wp.pl>
2021-03-22 13:12:31 +0100

    Blog - config

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/App.module.scss";
+
+const App = () => {
+    return (
+        <>
+            <h1 className={styles.headers}>Hello world</h1>
+        </>
+    );
+};
+
+export default App;
